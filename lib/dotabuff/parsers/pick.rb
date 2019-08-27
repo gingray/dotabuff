595ed4c187ab78ab55cd8cb9160176505b4a36d3
Ivan Lopatin <gingray.dev@gmail.com>
2019-08-27 16:05:40 +0200

    minor improvements

--- lib/dotabuff/parsers/pick.rb
+++ lib/dotabuff/parsers/pick.rb
@@ -1,8 +1,11 @@
 module Dotabuff
   module Parsers
     class Pick < Base
-      HERO_LIST = '.content-inner section:nth-of-type(3) table tbody tr'
-      WIN_RATE = '.header-content-container span.won'
+      HERO_LIST = '.content-inner section:nth-of-type(3) table tbody tr'.freeze
+      WIN_RATE = '.header-content-container span.won'.freeze
+      NAME = './td[1]'.freeze
+      WIN_SCORE = './td[3]'.freeze
+      WIN_RATE = './td[4]'.freeze
 
       def process
         hero_list = @doc.css HERO_LIST
@@ -10,9 +13,9 @@ module Dotabuff
         hash = { win_rate: win_rate.text }
         stat = []
         hero_list.each do |item|
-          name = item.xpath('./td[1]').first.attributes['data-value'].value
-          win_score = item.xpath('./td[3]').first.attributes['data-value'].value
-          win_rate = item.xpath('./td[4]').first.attributes['data-value'].value
+          name = item.xpath(NAME).first.attributes['data-value'].value
+          win_score = item.xpath(WIN_SCORE).first.attributes['data-value'].value
+          win_rate = item.xpath(WIN_RATE).first.attributes['data-value'].value
           hero = { name: name, win_score: win_score, win_rate: win_rate }
           stat << hero
         end
